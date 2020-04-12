cc1deba7682ebb4a5093b4127fceb9f1485a7d8d
Petra Kumi <pkumi@wpi.edu>
2020-04-12 15:20:33 -0400

    added footer to homepage

--- index_script.js
+++ index_script.js
@@ -34,9 +34,9 @@ let dept_arr = ['Actuarial Mathematics',
 ];
 window.onload = function () {
     load_grid();
-    window.onresize = function(){
-        if(window.innerWidth<518){
-            for (let e of document.getElementsByClassName('content-row')){
+    window.onresize = function () {
+        if (window.innerWidth < 518) {
+            for (let e of document.getElementsByClassName('content-row')) {
                 e.style.height = '100%'
             }
         }
@@ -45,18 +45,19 @@ window.onload = function () {
 
 
 function load_grid() {
-    let body_tag = document.getElementById('append-grid');
+    let container_tag = document.getElementById('append-grid');
     for (let i = 0; i < Math.ceil(dept_arr.length / 4); i++) {
         let row = document.createElement('div');
         row.classList.add('row');
         row.classList.add('no-gutters');
+        row.classList.add('px-3');
         row.classList.add('content-row');
         row.style.minHeight = "25vh";
         row.style.width = "100vw";
 
         for (let j = 0; j < 4; j++) {
-                            let idx = (i * 4) + j;
-            if ( idx < dept_arr.length) {
+            let idx = (i * 4) + j;
+            if (idx < dept_arr.length) {
                 let col = document.createElement('div');
                 col.classList.add('col-md-3');
                 // col.classList.add('px-1');
@@ -74,7 +75,7 @@ function load_grid() {
 
                 let title_row = document.createElement('div');
                 title_row.classList.add('centered');
-                title_row.innerHTML = "<h3 id='h-"+String(idx)+"' style='font-size: 1.3em'>" + dept_arr[idx] + "</h3>";
+                title_row.innerHTML = "<h3 id='h-" + String(idx) + "' style='font-size: 1.3em'>" + dept_arr[idx] + "</h3>";
 
                 // title_row.style.fontSize = '0.5em';
 
@@ -96,29 +97,47 @@ function load_grid() {
                 button.addEventListener('mouseover', function () {
                     // document.getElementById('i-'+String(idx)).style.transition = 'transform 1.3s';
                     // document.getElementById('i-'+String(idx)).style.transform = 'scale (1.5)';
-                    document.getElementById('h-'+String(idx)).style.transition = 'font-size 0.3s';
-                    document.getElementById('h-'+String(idx)).style.fontSize = '1.5em';
+                    document.getElementById('h-' + String(idx)).style.transition = 'font-size 0.3s';
+                    document.getElementById('h-' + String(idx)).style.fontSize = '1.5em';
                 });
                 button.addEventListener('mouseout', function () {
                     img.style.transition = 'transform 1.3s';
                     img.style.transform = 'scale (1)';
-                    document.getElementById('h-'+String(idx)).style.transition = 'font-size 0.3s';
-                    document.getElementById('h-'+String(idx)).style.fontSize = '1.3em';
+                    document.getElementById('h-' + String(idx)).style.transition = 'font-size 0.3s';
+                    document.getElementById('h-' + String(idx)).style.fontSize = '1.3em';
                 })
             }
         }
 
-        body_tag.appendChild(row);
+        container_tag.appendChild(row);
     }
-        // setTimeout(function(){
+    // setTimeout(function(){
     //     AOS.init()
     // },1000)
+    load_footer();
 
 }
 
-function generate_page_name(input){
+function generate_page_name(input) {
     input = input.replace(/ /g, "-");
-    input = input.replace(/'/g,"");
-    input = input.replace(/,/g,"");
-    return 'dept-pages/'+input.toLowerCase()+'.html';
+    input = input.replace(/'/g, "");
+    input = input.replace(/,/g, "");
+    return 'dept-pages/' + input.toLowerCase() + '.html';
+}
+
+function load_footer() {
+    let footer = document.createElement('footer');
+    footer.setAttribute('id', 'footer');
+    footer.classList.add('page-footer');
+    footer.classList.add('font-small');
+    footer.classList.add('white');
+
+    let adiv = document.createElement('div');
+    adiv.classList.add('footer-copyright');
+    adiv.classList.add('text-center');
+    adiv.classList.add('py-3');
+    adiv.innerHTML = 'Worcester Polytechnic Institute <a href="https://www.wpi.edu"> wpi.edu</a>';
+
+    footer.appendChild(adiv);
+    document.getElementsByTagName('body')[0].appendChild(footer)
 }
